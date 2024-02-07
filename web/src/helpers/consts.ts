c6738af99ae85ec732ebe9ab9d2f64234379dae4
Kazuki H <kazukih0205@gmail.com>
2024-02-07 18:06:33 +0900

    web: Load 200 memos by default

--- web/src/helpers/consts.ts
+++ web/src/helpers/consts.ts
@@ -8,4 +8,4 @@ export const DAILY_TIMESTAMP = 3600 * 24 * 1000;
 export const TAB_SPACE_WIDTH = 2;
 
 // DEFAULT_LIST_MEMOS_PAGE_SIZE is the default page size for list memos request.
-export const DEFAULT_LIST_MEMOS_PAGE_SIZE = 16;
+export const DEFAULT_LIST_MEMOS_PAGE_SIZE = 200;
